f0435246e20e320c5ddce0a3c18c40454a5464be
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-14 21:42:49 +0300

    todoForm component created

--- src/components/todoForm/index.tsx
+++ src/components/todoForm/index.tsx
@@ -1,7 +1,19 @@
 import "./style.scss";
 
 const TodoForm: React.FC = () => {
-  return <div>form</div>;
+  return (
+    <div className="todo-form">
+      <div className="form-title">
+        <p className="title-text">What's the plan for today?</p>
+      </div>
+      <div className="form-container">
+        <form className="form">
+          <input className="form-input" placeholder="New todo" />
+          <button className="form-button">Add new task</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default TodoForm;
