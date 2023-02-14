aef5774563c07dd3a1d51448ad8d979d4d56c1c7
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-15 02:05:09 +0300

    delete todo function created

--- src/components/todoItem/index.tsx
+++ src/components/todoItem/index.tsx
@@ -2,12 +2,20 @@ import "./style.scss";
 
 import { todoList } from "../../types/type";
 import { RiEdit2Fill, RiDeleteBin5Fill } from "react-icons/ri";
+import { useAppDispatch } from "../../redux/hook";
+import { deleteTodo } from "../../redux/slices/todoSlice";
 
 type IProps = {
   item: todoList;
 };
 
 const TodoItem: React.FC<IProps> = ({ item }) => {
+  const dispatch = useAppDispatch();
+
+  const deleteButtonIconClick = () => {
+    dispatch(deleteTodo(item.id));
+  };
+
   return (
     <div className="todo-item">
       <div className="item-text">
@@ -18,7 +26,7 @@ const TodoItem: React.FC<IProps> = ({ item }) => {
         <button className="button-icon-edit">
           <RiEdit2Fill className="icon" />
         </button>
-        <button className="button-icon-delete">
+        <button className="button-icon-delete" onClick={deleteButtonIconClick}>
           <RiDeleteBin5Fill className="icon" />
         </button>
       </div>
