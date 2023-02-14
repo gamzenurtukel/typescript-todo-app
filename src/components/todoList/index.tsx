6778f4f700b6b09229f731784b2d04cf64438fa9
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-14 21:43:57 +0300

    todoList component created

--- src/components/todoList/index.tsx
+++ src/components/todoList/index.tsx
@@ -1,7 +1,19 @@
 import "./style.scss";
 
+import TodoFilterButtons from "../todoFilterButtons";
+
 const TodoList = () => {
-  return <div>todolist</div>;
+  return (
+    <div className="todo-list">
+      <div className="todo-list-title">
+        <p className="title-text">Todo list</p>
+      </div>
+      <div className="todo-list-row">
+        <TodoFilterButtons />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default TodoList;
