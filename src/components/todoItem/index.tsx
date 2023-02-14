b2bb24f7e513c7c8c59b14280202524a1fd3b63f
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-15 01:53:06 +0300

    todoItem styles done

--- src/components/todoItem/index.tsx
+++ src/components/todoItem/index.tsx
@@ -1,7 +1,29 @@
 import "./style.scss";
 
-const TodoItem: React.FC = () => {
-  return <div>item</div>;
+import { todoList } from "../../types/type";
+import { RiEdit2Fill, RiDeleteBin5Fill } from "react-icons/ri";
+
+type IProps = {
+  item: todoList;
+};
+
+const TodoItem: React.FC<IProps> = ({ item }) => {
+  return (
+    <div className="todo-item">
+      <div className="item-text">
+        <p>{item.value}</p>
+      </div>
+      <div className="item-buttons">
+        <input type="checkbox" className="input-checkbox" />
+        <button className="button-icon-edit">
+          <RiEdit2Fill className="icon" />
+        </button>
+        <button className="button-icon-delete">
+          <RiDeleteBin5Fill className="icon" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoItem;
