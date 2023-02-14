2c48f33ff16fd6e8c5e293d2879da65621b1d63f
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-15 02:04:36 +0300

    deleteTodo action created

--- src/redux/slices/todoSlice.tsx
+++ src/redux/slices/todoSlice.tsx
@@ -20,9 +20,12 @@ const todoSlice = createSlice({
         ...state.todoList,
       ];
     },
+    deleteTodo: (state, action: PayloadAction<string>) => {
+      state.todoList = state.todoList.filter((todo) => todo.id !== action.payload);
+    },
   },
 });
 
 export const getAllTodo = (state: RootState) => state.todos.todoList;
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo } = todoSlice.actions;
 export default todoSlice.reducer;
