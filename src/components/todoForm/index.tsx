2e2cd51d79baebf0728f6d47885aba0dcc0339c4
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-15 00:20:10 +0300

    add todo functions done

--- src/components/todoForm/index.tsx
+++ src/components/todoForm/index.tsx
@@ -1,6 +1,23 @@
 import "./style.scss";
 
+import { useState, ChangeEvent } from "react";
+import { addTodo } from "../../redux/slices/todoSlice";
+import { useAppDispatch } from "../../redux/hook";
+
 const TodoForm: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const dispatch = useAppDispatch();
+
+  const inputOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const buttonOnClick: React.MouseEventHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(inputValue));
+    setInputValue("");
+  };
+
   return (
     <div className="todo-form">
       <div className="form-title">
@@ -8,8 +25,15 @@ const TodoForm: React.FC = () => {
       </div>
       <div className="form-container">
         <form className="form">
-          <input className="form-input" placeholder="New todo" />
-          <button className="form-button">Add new task</button>
+          <input
+            className="form-input"
+            placeholder="New todo"
+            value={inputValue}
+            onChange={inputOnChange}
+          />
+          <button className="form-button" onClick={buttonOnClick}>
+            Add new task
+          </button>
         </form>
       </div>
     </div>
