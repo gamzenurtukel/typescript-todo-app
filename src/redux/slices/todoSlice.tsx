882182c0147eb0483e7f7f6e158fd119b1a14b6d
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-15 17:15:25 +0300

    complete action created

--- src/redux/slices/todoSlice.tsx
+++ src/redux/slices/todoSlice.tsx
@@ -4,6 +4,7 @@ import { RootState } from "../store";
 
 const initialState = {
   todoList: [] as todoList[],
+  completeTodoList: [] as todoList[],
 };
 
 const todoSlice = createSlice({
@@ -23,9 +24,24 @@ const todoSlice = createSlice({
     deleteTodo: (state, action: PayloadAction<string>) => {
       state.todoList = state.todoList.filter((todo) => todo.id !== action.payload);
     },
+    completeTodo: (state, action) => {
+      state.todoList.map((todo) => {
+        if (todo.id === action.payload) {
+          todo.isDone = !todo.isDone;
+          state.completeTodoList.push(todo);
+          if (!todo.isDone) {
+            state.completeTodoList = state.completeTodoList.filter(
+              (item) => item.id !== todo.id
+            );
+          }
+        }
+        return todo;
+      });
+    },
   },
 });
 
+export const getDoneTodos = (state: RootState) => state.todos.completeTodoList;
 export const getAllTodo = (state: RootState) => state.todos.todoList;
-export const { addTodo, deleteTodo } = todoSlice.actions;
+export const { addTodo, deleteTodo, completeTodo } = todoSlice.actions;
 export default todoSlice.reducer;
