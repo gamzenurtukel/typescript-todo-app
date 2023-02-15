296c990e2870e53f3da9be69148026242f40833d
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-15 22:11:18 +0300

    notCompleteTodoList operations created

--- src/redux/slices/todoSlice.tsx
+++ src/redux/slices/todoSlice.tsx
@@ -5,6 +5,7 @@ import { RootState } from "../store";
 const initialState = {
   todoList: [] as todoList[],
   completeTodoList: [] as todoList[],
+  notComplateTodoList: [] as todoList[],
 };
 
 const todoSlice = createSlice({
@@ -20,19 +21,35 @@ const todoSlice = createSlice({
         },
         ...state.todoList,
       ];
+      state.todoList.map((x) => {
+        const todo = state.notComplateTodoList.find((item) => item.id === x.id);
+        if (!todo) {
+          state.notComplateTodoList.push(x);
+        }
+      });
     },
     deleteTodo: (state, action: PayloadAction<string>) => {
       state.todoList = state.todoList.filter((todo) => todo.id !== action.payload);
+      state.completeTodoList = state.completeTodoList.filter(
+        (todo) => todo.id !== action.payload
+      );
+      state.notComplateTodoList = state.notComplateTodoList.filter(
+        (todo) => todo.id !== action.payload
+      );
     },
     completeTodo: (state, action) => {
       state.todoList.map((todo) => {
         if (todo.id === action.payload) {
           todo.isDone = !todo.isDone;
           state.completeTodoList.push(todo);
+          state.notComplateTodoList = state.notComplateTodoList.filter(
+            (item) => item.id !== todo.id
+          );
           if (!todo.isDone) {
             state.completeTodoList = state.completeTodoList.filter(
               (item) => item.id !== todo.id
             );
+            state.notComplateTodoList.push(todo);
           }
         }
         return todo;
@@ -40,7 +57,8 @@ const todoSlice = createSlice({
     },
   },
 });
-
+export const getNotIsDoneTodos = (state: RootState) =>
+  state.todos.notComplateTodoList;
 export const getDoneTodos = (state: RootState) => state.todos.completeTodoList;
 export const getAllTodo = (state: RootState) => state.todos.todoList;
 export const { addTodo, deleteTodo, completeTodo } = todoSlice.actions;
