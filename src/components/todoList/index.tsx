09b93e79f2a09a0ee00299959ed299aea6bf0dba
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-15 22:08:15 +0300

    filter buttons fuction created

--- src/components/todoList/index.tsx
+++ src/components/todoList/index.tsx
@@ -4,19 +4,71 @@ import TodoFilterButtons from "../todoFilterButtons";
 import { getAllTodo } from "../../redux/slices/todoSlice";
 import { useAppSelector } from "../../redux/hook";
 import TodoItem from "../todoItem";
+import { useEffect, useState } from "react";
+import { todoList } from "../../types/type";
+import { getDoneTodos, getNotIsDoneTodos } from "../../redux/slices/todoSlice";
 
 const TodoList = () => {
+  const [todoListData, setTodoListData] = useState<todoList[]>();
+  const [doneClick, setDoneClick] = useState<boolean>(false);
+  const [notIsDoneClick, setnotIsDoneClick] = useState<boolean>(false);
+  const [allClick, setAllClick] = useState<boolean>(false);
+
+  const notIsDoneTodoList = useAppSelector(getNotIsDoneTodos);
   const todoList = useAppSelector(getAllTodo);
+  const doneTodoList = useAppSelector(getDoneTodos);
+
+  useEffect(() => {
+    setTodoListData(todoList);
+    if (doneClick) {
+      setTodoListData(doneTodoList);
+    }
+    if (allClick) {
+      setTodoListData(todoList);
+    }
+    if (notIsDoneClick) {
+      setTodoListData(notIsDoneTodoList);
+    }
+  }, [
+    todoListData,
+    todoList,
+    doneTodoList,
+    doneClick,
+    allClick,
+    notIsDoneClick,
+    notIsDoneTodoList,
+  ]);
+
+  const doneButtonOnClick = () => {
+    setDoneClick(true);
+    setAllClick(false);
+    setnotIsDoneClick(false);
+  };
+  const allButtonOnClick = () => {
+    setAllClick(true);
+    setDoneClick(false);
+    setnotIsDoneClick(false);
+  };
+  const todoButtonOnClick = () => {
+    setnotIsDoneClick(true);
+    setAllClick(false);
+    setDoneClick(false);
+  };
+
   return (
     <div className="todo-list">
       <div className="todo-list-title">
         <p className="title-text">Todo list</p>
       </div>
       <div className="todo-list-row">
-        <TodoFilterButtons />
+        <TodoFilterButtons
+          doneButtonClick={doneButtonOnClick}
+          allButtonOnClick={allButtonOnClick}
+          todoButtonOnClick={todoButtonOnClick}
+        />
       </div>
       <div>
-        {todoList?.map((item) => (
+        {todoListData?.map((item) => (
           <TodoItem key={item.id} item={item} />
         ))}
       </div>
