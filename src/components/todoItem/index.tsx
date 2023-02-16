43b4af8edcea3bdd0dfc981fc82ee9bf3f878069
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-16 20:10:21 +0300

    edit todo created

--- src/components/todoItem/index.tsx
+++ src/components/todoItem/index.tsx
@@ -3,14 +3,22 @@ import "./style.scss";
 import { todoList } from "../../types/type";
 import { RiEdit2Fill, RiDeleteBin5Fill } from "react-icons/ri";
 import { useAppDispatch } from "../../redux/hook";
-import { deleteTodo, completeTodo } from "../../redux/slices/todoSlice";
+import {
+  deleteTodo,
+  completeTodo,
+  editTodo,
+  setTodo,
+} from "../../redux/slices/todoSlice";
 import classNames from "classnames";
+import { useState, ChangeEvent } from "react";
 
 type IProps = {
   item: todoList;
 };
 
 const TodoItem: React.FC<IProps> = ({ item }) => {
+  const [editButtonClick, setEditButtonClick] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>(item.value);
   const dispatch = useAppDispatch();
 
   const deleteButtonIconClick = () => {
@@ -20,19 +28,45 @@ const TodoItem: React.FC<IProps> = ({ item }) => {
   const completeInputCheckboxClick = () => {
     dispatch(completeTodo(item.id));
   };
+  const editButtonIconClick = () => {
+    setEditButtonClick(true);
+    dispatch(setTodo(item));
+  };
+
+  const inputOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+  const saveFormButtonOnClick = () => {
+    dispatch(editTodo(inputValue));
+    setEditButtonClick(false);
+  };
 
   return (
     <div className="todo-item">
       <div className="item-text">
-        <p className={classNames({ "is-check": item.isDone })}>{item.value}</p>
+        {editButtonClick ? (
+          <form className="form-edit">
+            <input
+              className={classNames({ "form-input": editButtonClick })}
+              value={inputValue}
+              onChange={inputOnChange}
+            />
+            <button className="form-button" onClick={saveFormButtonOnClick}>
+              save
+            </button>
+          </form>
+        ) : (
+          <p className={classNames({ "is-check": item.isDone })}>{item.value}</p>
+        )}
       </div>
       <div className="item-buttons">
         <input
           type="checkbox"
           className="input-checkbox"
+          defaultChecked={item.isDone}
           onClick={completeInputCheckboxClick}
         />
-        <button className="button-icon-edit">
+        <button className="button-icon-edit" onClick={editButtonIconClick}>
           <RiEdit2Fill className="icon" />
         </button>
         <button className="button-icon-delete" onClick={deleteButtonIconClick}>
